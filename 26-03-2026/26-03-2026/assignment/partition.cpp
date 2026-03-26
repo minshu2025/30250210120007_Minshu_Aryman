b60893e277895cab176856b9c8180ef03e16380c
Minshu Kumar <dubeyminshu4@gmail.com>
2026-03-27 02:43:53 +0530

    Refactor partition function and remove unused code

--- 26-03-2026/26-03-2026/assignment/partition.cpp
+++ 26-03-2026/26-03-2026/assignment/partition.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
-using namespace std;
-
-// Definition of ListNode
-struct ListNode {
-    int val;
-    ListNode* next;
-
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
-
-// Solution class
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
@@ -38,50 +34,3 @@ public:
         return small->next;
     }
 };
-
-// Function to insert node at end
-ListNode* insert(ListNode* head, int val) {
-    if (head == nullptr) return new ListNode(val);
-
-    ListNode* temp = head;
-    while (temp->next != nullptr) {
-        temp = temp->next;
-    }
-    temp->next = new ListNode(val);
-    return head;
-}
-
-// Function to print list
-void printList(ListNode* head) {
-    while (head != nullptr) {
-        cout << head->val << " -> ";
-        head = head->next;
-    }
-    cout << "NULL" << endl;
-}
-
-// Main function
-int main() {
-    ListNode* head = nullptr;
-
-    // Example input: 1 4 3 2 5 2
-    int arr[] = {1, 4, 3, 2, 5, 2};
-    int n = 6;
-
-    for (int i = 0; i < n; i++) {
-        head = insert(head, arr[i]);
-    }
-
-    cout << "Original List: ";
-    printList(head);
-
-    int x = 3;
-
-    Solution obj;
-    head = obj.partition(head, x);
-
-    cout << "Partitioned List around " << x << ": ";
-    printList(head);
-
-    return 0;
-}
